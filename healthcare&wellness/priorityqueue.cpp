a268bee91621e5c880db595a7a815c9847f1fe50
poojaganeshbatti <poojaganeshbatti@gmail.com>
2025-12-24 16:08:27 +0530

    Update priorityqueue.cpp

--- healthcare&wellness/priorityqueue.cpp
+++ healthcare&wellness/priorityqueue.cpp
@@ -1,1 +1,9 @@
+import heapq
+
+patients = [(5, 'Patient1'), (10, 'Patient2'), (2, 'Patient3')] # Higher number = higher urgency
+heapq._heapify_max(patients)
+
+while patients:
+    urgency, name = heapq._heappop_max(patients)
+    print(name, urgency)
 
