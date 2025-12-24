9792d623d3e6cb13c7943d6083352c4b47865e87
poojaganeshbatti <poojaganeshbatti@gmail.com>
2025-12-24 21:55:10 +0530

    Create dijkstra's.cpp

---
+++ dijkstra's.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dijkstra(int src, int V, vector<vector<pair<int,int>>> &graph) {
+    vector<int> dist(V, INT_MAX);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (auto edge : graph[u]) {
+            int v = edge.first;
+            int weight = edge.second;
+
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    cout << "Shortest fuel-efficient routes from source:\n";
+    for (int i = 0; i < V; i++)
+        cout << "Intersection " << i << " → Distance: " << dist[i] << endl;
+}
+
+int main() {
+    int V = 5;
+    vector<vector<pair<int,int>>> graph(V);
+
+    graph[0].push_back({1, 4});
+    graph[0].push_back({2, 1});
+    graph[2].push_back({1, 2});
+    graph[1].push_back({3, 1});
+    graph[3].push_back({4, 3});
+
+    dijkstra(0, V, graph);
+    return 0;
+}
