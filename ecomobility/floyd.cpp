68ab3e4cd7654642a4770295f59303f062b26a84
poojaganeshbatti <poojaganeshbatti@gmail.com>
2025-12-24 22:02:05 +0530

    Create floyd.cpp

---
+++ ecomobility/floyd.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 99999
+
+int main() {
+    int V = 4;
+    int dist[V][V] = {
+        {0,   5,   INF, 10},
+        {INF, 0,   3,   INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    for (int k = 0; k < V; k++)
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
+    cout << "All-pairs shortest paths:\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++)
+            cout << dist[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
